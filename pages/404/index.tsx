37e1c23611db6f007bcc0db0050ae072edfae02d
izzatmammadov <imamedov388@gmail.com>
2024-01-15 16:55:44 +0400

    navbar and faq's props

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import { Navbar } from "../../share/components/Navbar";
 import { Footer } from "../../share/components/Footer";
+import Image from "next/image";
 
 
 const ErrorPage = () => {
   return (
     <>
-      <Navbar />
-      <main className=" flex justify-center items-center p-4" >
-       <img src="/errorImage.svg" alt="error 404" />
+      <Navbar isRegister={true}/>
+      <main className=" flex justify-center items-center p-4 mb-8" >
+       <Image className="w-full" width={0} height={0} src={"errorImage.svg"} alt="error"/>
       </main>
       <Footer isTop={false} />
     </>
--- pages/Faq/index.tsx
+++ pages/Faq/index.tsx
@@ -1,87 +1,136 @@
-import React from "react";
-import styls from "./style.module.css";
+import React, { useState } from "react";
 import { Navbar } from "../../share/components/Navbar";
 import { Footer } from "../../share/components/Footer";
+import Image from "next/image";
 
 const Faq = () => {
+  const [isContentVisible, setContentVisibility] = useState(false);
+
+  const toggleContent = () => {
+    setContentVisibility(!isContentVisible);
+  };
   return (
     <>
-      <Navbar />
-      <main className=" flex flex-col items-center gap-5 mt-[91px]">
-        <p className=" text-[45px] font-[500] pb-[21px]">F.A.Q</p>
-        <div className=" p-6 max-w-[1041px]  bg-white shadow-md  ">
+      <Navbar isRegister={true} />
+      <main className=" flex flex-col items-center gap-5 mb-8">
+        <p className=" text-5xl font-medium pb-5">F.A.Q</p>
+
+        <div className=" p-6 w-3/4 mx-auto  bg-white shadow-md">
           <div className="flex justify-between mb-3">
-            <p className={styls.FaqDivHeader}>
+            <p className=" text-2xl font-medium">
               How to contact with Customer Service?
-            </p>{" "}
-            <button>
-              <img src="/plus.svg" alt="" />
+            </p>
+            <button onClick={toggleContent}>
+              <Image
+                width={25}
+                height={0}
+                src={isContentVisible ? "plus.svg" : "minus.svg"}
+                alt={isContentVisible ? "plus" : "minus"}
+              />
             </button>
           </div>
-          <div className={styls.FaqDivMain}>
-            Our Customer Experience Team is available 7 days a week and we offer
-            2 ways to get in contact.Email and Chat . We try to reply quickly,
-            so you need not to wait too long for a response!.
-          </div>
-        </div>
-        <div className=" p-6 max-w-[1041px]  bg-white shadow-md  ">
-          <div className="flex justify-between mb-3">
-            <p className={styls.FaqDivHeader}>
-              App installation failed, how to update system information?{" "}
-            </p>{" "}
-            <button>
-              <img src="/plus.svg" alt="" />
-            </button>
-          </div>
-          <div className={styls.FaqDivMain}>
-            Our Customer Experience Team is available 7 days a week and we offer
-            2 ways to get in contact.Email and Chat . We try to reply quickly,
-            so you need not to wait too long for a response!.
-          </div>
-        </div>
-        <div className=" p-6 max-w-[1041px]  bg-white shadow-md  ">
-          <div className="flex justify-between mb-3">
-            <p className={styls.FaqDivHeader}>
-              Website reponse taking time, how to improve?
-            </p>{" "}
-            <button>
-              <img src="/plus.svg" alt="" />
-            </button>
-          </div>
-          <div className={styls.FaqDivMain}>
-            Our Customer Experience Team is available 7 days a week and we offer
-            2 ways to get in contact.Email and Chat . We try to reply quickly,
-            so you need not to wait too long for a response!.
-          </div>
-        </div>
-        <div className=" p-6 max-w-[1041px]  bg-white shadow-md  ">
-          <div className="flex justify-between mb-3">
-            <p className={styls.FaqDivHeader}>How do I create a account? </p>{" "}
-            <button>
-              <img src="/plus.svg" alt="" />
-            </button>
-          </div>
-          <div className={styls.FaqDivMain}>
-            Our Customer Experience Team is available 7 days a week and we offer
-            2 ways to get in contact.Email and Chat . We try to reply quickly,
-            so you need not to wait too long for a response!.
-          </div>
+
+          {isContentVisible && (
+            <div className="text-grayText1 text-lg leading-7 font-medium">
+              Our Customer Experience Team is available 7 days a week and we
+              offer 2 ways to get in contact. Email and Chat. We try to reply
+              quickly, so you need not to wait too long for a response!.
+            </div>
+          )}
         </div>
 
-        <div className=" p-6 max-w-[1041px]  bg-white shadow-md  ">
+        <div className=" p-6 w-3/4 mx-auto  bg-white shadow-md">
           <div className="flex justify-between mb-3">
-            <p className={styls.FaqDivHeader}>
-              Website reponse taking time, how to improve?
-            </p>{" "}
-            <button>
-              <img src="/plus.svg" alt="" />
+            <p className=" text-2xl font-medium">
+              App installation failed, how to update system information?
+            </p>
+            <button onClick={toggleContent}>
+              <Image
+                width={25}
+                height={0}
+                src={isContentVisible ? "plus.svg" : "minus.svg"}
+                alt={isContentVisible ? "plus" : "minus"}
+              />
             </button>
           </div>
-          <div className={styls.FaqDivMain}>
-            Our Customer Experience Team is available 7 days a week and we offer
-            2 ways to get in contact.Email and Chat . We try to reply quickly,
-            so you need not to wait too long for a response!.
+
+          {isContentVisible && (
+            <div className="text-grayText1 text-lg leading-7 font-medium">
+              Our Customer Experience Team is available 7 days a week and we
+              offer 2 ways to get in contact. Email and Chat. We try to reply
+              quickly, so you need not to wait too long for a response!.
+            </div>
+          )}
+        </div>
+
+        <div className=" p-6 w-3/4 mx-auto  bg-white shadow-md">
+          <div className="flex justify-between mb-3">
+            <p className=" text-2xl font-medium">
+              Website reponse taking time, how to improve?
+            </p>
+            <button onClick={toggleContent}>
+              <Image
+                width={25}
+                height={0}
+                src={isContentVisible ? "plus.svg" : "minus.svg"}
+                alt={isContentVisible ? "plus" : "minus"}
+              />
+            </button>
           </div>
+
+          {isContentVisible && (
+            <div className="text-grayText1 text-lg leading-7 font-medium">
+              Our Customer Experience Team is available 7 days a week and we
+              offer 2 ways to get in contact. Email and Chat. We try to reply
+              quickly, so you need not to wait too long for a response!.
+            </div>
+          )}
+        </div>
+
+        <div className=" p-6 w-3/4 mx-auto  bg-white shadow-md">
+          <div className="flex justify-between mb-3">
+            <p className=" text-2xl font-medium">How do I create a account?</p>
+            <button onClick={toggleContent}>
+              <Image
+                width={25}
+                height={0}
+                src={isContentVisible ? "plus.svg" : "minus.svg"}
+                alt={isContentVisible ? "plus" : "minus"}
+              />
+            </button>
+          </div>
+
+          {isContentVisible && (
+            <div className="text-grayText1 text-lg leading-7 font-medium">
+              Our Customer Experience Team is available 7 days a week and we
+              offer 2 ways to get in contact. Email and Chat. We try to reply
+              quickly, so you need not to wait too long for a response!.
+            </div>
+          )}
+        </div>
+
+        <div className=" p-6 w-3/4 mx-auto  bg-white shadow-md">
+          <div className="flex justify-between mb-3">
+            <p className=" text-2xl font-medium">
+              Website reponse taking time, how to improve?
+            </p>
+            <button onClick={toggleContent}>
+              <Image
+                width={25}
+                height={0}
+                src={isContentVisible ? "plus.svg" : "minus.svg"}
+                alt={isContentVisible ? "plus" : "minus"}
+              />
+            </button>
+          </div>
+
+          {isContentVisible && (
+            <div className="text-grayText1 text-lg leading-7 font-medium">
+              Our Customer Experience Team is available 7 days a week and we
+              offer 2 ways to get in contact. Email and Chat. We try to reply
+              quickly, so you need not to wait too long for a response!.
+            </div>
+          )}
         </div>
       </main>
       <Footer isTop={false} />
