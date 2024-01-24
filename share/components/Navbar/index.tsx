1a44b511b20fac19a43a1e12147c4df2b792e1af
izzatmammadov <imamedov388@gmail.com>
2024-01-24 21:18:50 +0400

    home responsived and hamburger menu

--- share/components/Navbar/index.tsx
+++ share/components/Navbar/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { Button } from "../Button";
-import  {Langugages}  from "../Languages/index";
+import { Langugages } from "../Languages/index";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 interface NavbarParamTypes {
   isRegister: boolean | null;
@@ -16,7 +17,13 @@ export const Navbar = ({
   forAdmin,
 }: NavbarParamTypes) => {
   const { t, i18n } = useTranslation();
-  const navigate = useRouter()
+  const navigate = useRouter();
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
 
   return (
     <nav
@@ -24,9 +31,13 @@ export const Navbar = ({
         forLogin ? "bg-lightRed" : "bg-whiteLight1"
       } rounded-md py-11 sm:p-11`}
     >
-      <h1 className={`text-4xl font-extrabold ${forLogin ? "text-white" : ""} flex items-center`}>
-        <button className="sm:hidden block mr-5">
-        <Image width={40} height={0} src={"hamburger.svg"} alt="hamburger"/>
+      <h1
+        className={`text-4xl font-extrabold ${
+          forLogin ? "text-white" : ""
+        } flex items-center`}
+      >
+        <button onClick={toggleModal} className="sm:hidden block mr-5">
+          <Image width={40} height={0} src={"hamburger.svg"} alt="hamburger" />
         </button>
         Foody
         <span className={`${forLogin ? "text-white" : "text-mainRed"} `}>
@@ -39,19 +50,34 @@ export const Navbar = ({
       ) : (
         <>
           <ul className="justify-around text-lg w-1/2 font-medium text-grayText1 hidden sm:flex ">
-            <li onClick={()=>navigate.push("/")} className="cursor-pointer hover:text-mainRed transition-all">
-            {t("home")}
+            <li
+              onClick={() => navigate.push("/")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("home")}
             </li>
-            <li onClick={()=>navigate.push("/restaurants")} className="cursor-pointer hover:text-mainRed transition-all">
-            {t("restaurants")}
+            <li
+              onClick={() => navigate.push("/restaurants")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("restaurants")}
             </li>
-            <li onClick={()=>navigate.push("/about-us")} className="cursor-pointer hover:text-mainRed transition-all">
-            {t("about_us")}
+            <li
+              onClick={() => navigate.push("/about-us")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("about_us")}
             </li>
-            <li onClick={()=>navigate.push("/how-it-works")} className="cursor-pointer hover:text-mainRed transition-all">
-            {t("how_it_works")}
+            <li
+              onClick={() => navigate.push("/how-it-works")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("how_it_works")}
             </li>
-            <li onClick={()=>navigate.push("/faqs")} className="cursor-pointer hover:text-mainRed transition-all">
+            <li
+              onClick={() => navigate.push("/faqs")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
               FAQs
             </li>
           </ul>
@@ -105,6 +131,48 @@ export const Navbar = ({
             />
           </div>
         </>
+      )}
+
+{isModalOpen && (
+        <div className="fixed inset-0 bg-black m-8 bg-opacity-50 z-50 flex items-center justify-start">
+          {/* MODAL BASLAYIR*/}
+          <div className="bg-white w-4/5 min-h-full p-8 flex flex-col">
+          <button className="text-mainRed font-bold text-lg block text-start" onClick={toggleModal}><Image width={30} height={0} alt="close2" src={"close2.svg"}/></button>
+            <Button className=" w-1/2 mx-auto py-4 rounded-full text-xl bg-mainRed text-white font-medium shadow-md hover:scale-95 transition-all duration-500" innerText={t("signUp")}/>
+            <ul className="justify-around text-2xl w-1/2 font-medium text-grayText1 flex flex-col mt-20 gap-4 ">
+            <li
+              onClick={() => navigate.push("/")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("home")}
+            </li>
+            <li
+              onClick={() => navigate.push("/restaurants")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("restaurants")}
+            </li>
+            <li
+              onClick={() => navigate.push("/about-us")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("about_us")}
+            </li>
+            <li
+              onClick={() => navigate.push("/how-it-works")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              {t("how_it_works")}
+            </li>
+            <li
+              onClick={() => navigate.push("/faqs")}
+              className="cursor-pointer hover:text-mainRed transition-all"
+            >
+              FAQs
+            </li>
+          </ul>
+          </div>
+        </div>
       )}
     </nav>
   );
