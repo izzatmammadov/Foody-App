a4ade350ad8b29ee57c15fe823c25675c074d46d
izzatmammadov <imamedov388@gmail.com>
2024-01-26 18:10:01 +0400

    responsive

--- pages/userOrder/index.tsx
+++ pages/userOrder/index.tsx
@@ -21,10 +21,10 @@ const UserOrder: NextPage = () => {
       <main>
         <Navbar isRegister={true} forAdmin={null} forLogin={null} />
 
-        <section className="m-8 flex justify-center gap-10">
+        <section className="mx-0 sm:m-8 flex justify-center gap-10">
           <UserAside/>
 
-          <div className="w-full flex  flex-col justify-between px-8 py-10 flex-wrap gap-x-1 gap-y-8 bg-whiteLight1">
+          <div className="w-full flex  flex-col justify-between px-8 py-10 flex-wrap gap-x-1 gap-y-8 sm:bg-whiteLight1">
             <h2 className=" font-semibold text-3xl text-grayText2">
               {t("userDesc3")}
             </h2>
