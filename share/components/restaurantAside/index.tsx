9d4a98d77560a3c14bf38125fad555b6f8228e05
izzatmammadov <imamedov388@gmail.com>
2024-01-27 13:16:55 +0400

    filter modal changed to component

---
+++ share/components/restaurantAside/index.tsx
@@ -0,0 +1,178 @@
+import Image from "next/image"
+
+
+export const RestaurantAside = () => {
+    return (
+        <>
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer p-1 hover:bg-mainRedLight transition-all">
+          <Image
+            width={40}
+            height={0}
+            src={"margaritaCard.svg"}
+            alt="margarita"
+          />
+          <p className="font-semibold text-[20px] ">Fast Food</p>
+        </div>
+        </>
+    )
+}
