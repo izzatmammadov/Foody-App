f49a7d506e45e853b81f1d98bde5b02ad565d80c
izzatmammadov <imamedov388@gmail.com>
2024-01-31 13:41:44 +0400

    with axios and useRef: check register and log in

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,19 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import { Button } from "../../share/components/Button";
 import { Navbar } from "../../share/components/Navbar";
-import { useTranslation } from "react-i18next";
+import { ClientLogInForm } from "@/share/components/clientLoginForm";
 
 const Login: NextPage = () => {
-  const [showLoginForm, setShowLoginForm] = useState(true);
-  const { t, i18n } = useTranslation();
-
-
-  const switchForm = () => {
-    setShowLoginForm(!showLoginForm);
-  };
+ 
   return (
     <>
       <Head>
@@ -25,114 +16,7 @@ const Login: NextPage = () => {
       <Navbar isRegister={false} forLogin={true} forAdmin={true} />
 
       <main>
-        <section className="flex flex-col mt-5 sm:mt-0 sm:flex-row justify-center bg-white sm:bg-lightRed mx-8 mb-8 ">
-          <div className="bg-lightRed m-auto w-full sm:w-1/2">
-            <Image
-              className="w-4/5 m-auto"
-              width={0}
-              height={0}
-              src={showLoginForm ? "loginClient.svg" : "registerClient.svg"}
-              alt={showLoginForm ? "loginClient.svg" : "registerClient.svg"}
-            />
-          </div>
-          {/* FORM DIV */}
-          <div className="w-full sm:w-1/2 flex flex-col gap-20 bg-white py-12 mx-auto">
-            <div className="flex gap-16 mx-auto w-max">
-              <button
-                className={`text-3xl ${
-                  showLoginForm ? "text-lightRed border-b-4" : "text-grayText1"
-                } font-medium`}
-                onClick={() => switchForm()}
-              >
-                {t("login")}
-              </button>
-              <button
-                className={`text-3xl ${
-                  showLoginForm ? "text-grayText1" : "text-lightRed border-b-4"
-                } font-medium`}
-                onClick={() => switchForm()}
-              >
-                {t("register")}
-              </button>
-            </div>
-
-            {showLoginForm ? (
-              <form className="flex flex-col w-full sm:w-4/5 mx-auto gap-8 items-center">
-                <div className="flex flex-col gap-2 w-full">
-                  <label className="text-xl font-medium text-grayText2">
-                    {t("username")}
-                  </label>
-                  <input
-                    className="p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="text"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2 w-full mb-8">
-                  <label className="text-xl font-medium text-grayText2">
-                    {t("password")}
-                  </label>
-                  <input
-                    className=" p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="password"
-                  />
-                </div>
-
-                <Button
-                  className="w-full bg-lightRed text-xl font-medium p-4 rounded-md text-white hover:scale-95 hover:bg-mainRed transition-all duration-500"
-                  innerText={t("login")}
-                />
-              </form>
-            ) : (
-              <form className="flex flex-col w-full sm:w-4/5 mx-auto gap-8 items-center">
-                <div className="flex flex-col gap-2 w-full">
-                  <label className="text-xl font-medium text-grayText2">
-                    {t("fullname")}
-                  </label>
-                  <input
-                    className="p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="text"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2 w-full">
-                  <label className="text-xl font-medium text-grayText2">
-                    {t("username")}
-                  </label>
-                  <input
-                    className=" p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="text"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2 w-full">
-                  <label className="text-xl font-medium text-grayText2">
-                    Email
-                  </label>
-                  <input
-                    className=" p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="e-mail"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2 w-full mb-8">
-                  <label className="text-xl font-medium text-grayText2">
-                    {t("password")}
-                  </label>
-                  <input
-                    className=" p-4 rounded-md outline-none bg-[#ffe6e6]"
-                    type="password"
-                  />
-                </div>
-
-                <Button
-                  className="w-full bg-lightRed text-xl font-medium p-4 rounded-md text-white hover:scale-95 hover:bg-mainRed transition-all duration-500"
-                  innerText={t("register")}
-                />
-              </form>
-            )}
-          </div>
-        </section>
+        <ClientLogInForm/>
       </main>
     </>
   );
--- share/components/input/index.tsx
+++ share/components/input/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React from "react";
 interface Props {
-    Type?: string
-    Placeholder?: string 
-    OnClick?: any
-    OnChange?: any
-    ClassName?: string
-    disabled?: boolean
-    Capitalize?:boolean
+  Type?: string;
+  Placeholder?: string;
+  OnClick?: any;
+  OnChange?: any;
+  ClassName?: string;
+  disabled?: boolean;
+  Capitalize?: boolean;
 }
-export const Input = ({Type="text",Placeholder,OnChange,OnClick,ClassName,disabled=false , Capitalize=true}:Props) => {
+export const Input = ({
+  Type = "text",
+  Placeholder,
+  OnChange,
+  OnClick,
+  ClassName,
+  disabled = false,
+  Capitalize = true,
+}: Props) => {
   return (
-    <><input className={`${Capitalize?"capitalize":" "}  ${ClassName}`} disabled={disabled} onClick={OnClick} onChange={OnChange} type={Type} placeholder={Placeholder} /></>
-  )
-}
+    <>
+      <input
+        className={`${Capitalize ? "capitalize" : " "}  ${ClassName}`}
+        disabled={disabled}
+        onClick={OnClick}
+        onChange={OnChange}
+        type={Type}
+        placeholder={Placeholder}
+      />
+    </>
+  );
+};
