818eed2e4f658b650686d4c3813a1df490e5e475
AsgerovSahib <asgerov.sako@gmail.com>
2024-02-04 23:06:38 +0400

    restourans api added and added firebase restouran item

--- share/services/axios.ts
+++ share/services/axios.ts
@@ -14,9 +14,18 @@ export type Form={
     "img_url": string|undefined
   }
 
+  
 export const postCategory = async (form: Form) => {
   try {
     const response = await instanceAxios.post("/category", form);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const postRestourans = async (form) => {
+  try {
+    const response = await instanceAxios.post("/restuarants", form);
     return response;
   } catch (err) {
     console.log(err);
