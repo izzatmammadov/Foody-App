4467a6f9c281d54a91f63f930561369bd167c52b
Aziz Azizli <azizliaziz025@gmail.com>
2024-02-07 23:59:00 +0400

    finished restaurant filter and fix some bugs

--- share/components/adminModalDropdown/index.tsx
+++ share/components/adminModalDropdown/index.tsx
@@ -5,47 +5,37 @@ interface Props {
   arr?: string[];
   className?: string;
   className2?: string;
-  useRef?: any;
   callBackValue?: any;
+  useRef?: any,
+  activeValue?:string
 }
 
 export const AdminModalDropdown = ({
   p = "Restaurants",
-
   arr = ["item1", "item2", "item3"],
-
   className,
-
-  useRef,
-
   className2,
-
   callBackValue,
+  useRef,
+  activeValue
 }: Props) => {
-  const [selectedValue, setSelectedValue] = useState<string | undefined>("");
+  // const [selectedValue, setSelectedValue] = useState<string | undefined>("");
 
-  // function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>): void {
-  //   const value = e.target.value;
-  //   setSelectedValue(value);
-  //   // console.log(value);
-  //   callBackValue(value);
-  // }
 
   return (
     <div className={className2}>
-      <p className=" text-white font-medium  text-base">{p}</p>
-      <select value={selectedValue} className={className}>
+      <p className="text-white font-medium text-base">{p}</p>
+      <select
+         ref={useRef}
+        value={activeValue}
+        className={className}
+        onChange={callBackValue}
+      >
         <option value="" disabled>
           Choose...
         </option>
         {arr.map((item, index) => (
-          <option
-            ref={useRef}
-            key={index}
-            className="cursor-pointer"
-            onClick={callBackValue}
-            value={item}
-          >
+          <option  key={index} value={item}>
             {item}
           </option>
         ))}
