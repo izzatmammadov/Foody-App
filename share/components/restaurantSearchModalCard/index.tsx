99ee807c8964ead9c056defa5bdb4a07fdf37b03
AsgerovSahib <asgerov.sako@gmail.com>
2024-02-16 18:02:55 +0400

    update search function for res pages

--- share/components/restaurantSearchModalCard/index.tsx
+++ share/components/restaurantSearchModalCard/index.tsx
@@ -4,11 +4,22 @@ interface Props {
   image: string;
   name: string;
   desc: string;
+  onclick: any;
+  id: any;
 }
 
-export const RestaurantSearchModalCard = ({ image, name, desc }: Props) => {
+export const RestaurantSearchModalCard = ({
+  image,
+  name,
+  desc,
+  onclick,
+  id,
+}: Props) => {
   return (
-    <div className="flex items-center gap-5 border-b-2 border-b-whiteLight1 p-4 mb-5 cursor-pointer hover:bg-whiteLight1">
+    <div
+      onClick={() => onclick(id)}
+      className="flex items-center gap-5 border-b-2 border-b-whiteLight1 p-4 mb-5 cursor-pointer hover:bg-whiteLight1"
+    >
       <Image width={60} height={0} src={image} alt={image} />
       <div className="flex flex-col w-full">
         <p className=" font-semibold">{name}</p>
