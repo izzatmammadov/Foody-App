32ef98bffdcf8a11ab64b1967d36f9606cbd2f71
AsgerovSahib <asgerov.sako@gmail.com>
2024-02-21 21:59:41 +0400

    reset firebase

--- server/configs/firebase.js
+++ server/configs/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnAbxfQHafE87R5hMnfHWp37dJ1XZF4gI",
-  authDomain: "foody2-58f3f.firebaseapp.com",
-  projectId: "foody2-58f3f",
-  storageBucket: "foody2-58f3f.appspot.com",
-  messagingSenderId: "956439257633",
-  appId: "1:956439257633:web:be5d64c33c8bad4102b960"
+  apiKey: "AIzaSyAsRyGh36w0mnsi7B9xOJSPqMwSTyOLuWE",
+  authDomain: "foody3-73b73.firebaseapp.com",
+  projectId: "foody3-73b73",
+  storageBucket: "foody3-73b73.appspot.com",
+  messagingSenderId: "115326302700",
+  appId: "1:115326302700:web:aec391285c1802bac8e8c9"
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
